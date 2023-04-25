a7bde5d5b07a9c32ccc52a98a6922ddd59aed74b
SaiBalaji2001 <gadisaibalaji2001@gmail.com>
2023-04-25 18:02:50 -0500

    Add Secured to update

--- routes/Drinks.js
+++ routes/Drinks.js
@@ -2,6 +2,16 @@ var express = require('express');
 Drinks_controller = require('../controllers/Drinks');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
+
 /* GET flowers */
 router.get("/", Drinks_controller.Drinks_view_all_Page);
 module.exports = router;
@@ -15,7 +25,7 @@ router.get("/detail", Drinks_controller.Drinks_view_one_Page);
 router.get("/create", Drinks_controller.Drinks_create_Page);
 
 /* GET create update page */
-router.get("/update", Drinks_controller.Drinks_update_Page);
+router.get("/update",secured, Drinks_controller.Drinks_update_Page);
 
 /* GET delete costume page */
 router.get("/delete", Drinks_controller.Drinks_delete_Page);
